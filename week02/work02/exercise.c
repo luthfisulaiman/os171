a796f2045adfe4bd9a1b2aeba73072edef1271d1
Luthfi Aulia Sulaiman - 1506757932 <luthfi.aulia@badak.cs.ui.ac.id>
2017-02-19 19:35:02 +0700

    Hamdalah Lab2 Kelar

---
+++ week02/work02/exercise.c
@@ -0,0 +1,17 @@
+/*
+* Name: [Luthfi Aulia Sulaiman]
+* NPM: [1506757932]
+* Class: [OS - D]
+* Comment: [Bismillah, lulus OS!]
+*/
+#define LOOP 4 //define me!
+#include <stdio.h>
+void main() {
+ int input, ii, THE_PRODUCT;
+ input = 5;
+ THE_PRODUCT = input * LOOP;
+ for (ii=0; ii<LOOP;ii++){
+  printf("%d times %d equals %d\n",input,LOOP,THE_PRODUCT);
+ }//define me!
+ // TO DO implement me!!
+}
